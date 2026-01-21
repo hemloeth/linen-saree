d5cb0ae40d282a313302bf8e031fb4f926f5e61c
Asrakash <asrakash41@gmail.com>
2026-01-22 00:19:07 +0530

    chnss

--- components/new-collections.tsx
+++ components/new-collections.tsx
@@ -29,7 +29,7 @@ export function NewCollections() {
         </div>
 
         {/* Products Grid */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 lg:gap-8">
+        <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-4 lg:grid-cols-4 gap-4 sm:gap-6 lg:gap-8">
           {newProducts.map((product) => (
             <ProductCard key={product.id} product={product} />
           ))}
