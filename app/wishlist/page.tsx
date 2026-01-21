0032dbc77250500969fd3cb8b188351dc0f51e27
Asrakash <asrakash41@gmail.com>
2026-01-21 22:16:42 +0530

    chnages

--- app/wishlist/page.tsx
+++ app/wishlist/page.tsx
@@ -39,7 +39,7 @@ export default function WishlistPage() {
                   onClick={clearWishlist}
                   className="hidden sm:flex"
                 >
-                  Clear Allaaaaaaaa
+                  Clear Allaaaaaaaadd
                 </Button>
                 <Button
                   onClick={handleAddAllToCart}
